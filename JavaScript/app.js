254f83c90c97c3e73ccd36d6d4d945a669292119
Eduard-Mychka <edmychka@gmail.com>
2020-04-30 19:47:04 +0300

    Photo Gallery

---
+++ JavaScript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(".button").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+    removalDelay: 400,
+    mainClass: "mfp-fade",
+  });
+});
+
+$(document).ready(function () {
+  $(".button_down").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+    removalDelay: 400,
+    mainClass: "mfp-fade",
+  });
+});
